4f324b1f7b578b1938dabbb3278de7e85d304661
Jaime Pineda <jaime.e.pineda@gmail.com>
2020-02-27 18:21:47 +0100

    Nnew config and plotting model

---
+++ config.py
@@ -0,0 +1,6 @@
+import astropy.units as u
+
+HC3N_TdV_10_9 = 'data/Per-emb-2-HC3N_10-9_TdV.fits'
+ra_Per2 = 15 * (3 + (32 + 17.92/60.) / 60.) * u.deg
+dec_Per2 = (30 + (49 + 48.03 / 60.) / 60.) * u.deg
+distance = 300. 
