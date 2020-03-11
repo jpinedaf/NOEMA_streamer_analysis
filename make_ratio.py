d96b443327460d42cd376ba90d277377496a4206
Jaime Pineda <jaime.e.pineda@gmail.com>
2020-03-11 02:39:16 +0100

    Fix bugs in make_ration.py

--- make_ratio.py
+++ make_ratio.py
@@ -21,6 +21,8 @@ hd8 = fits.getheader(HC3N_TdV_8_7)
 hd10 = fits.getheader(HC3N_TdV_10_9_match)
 hdu_R = fits.PrimaryHDU(Ratio, hd8)
 
+Pop_Up = 1.963E-02
+
 import velocity_tools.coordinate_offsets as c_offset
 results = c_offset.generate_offsets(hd8, ra_Per2, dec_Per2, pa_angle=0*u.deg, inclination=0*u.deg)
 
@@ -60,21 +62,29 @@ regions = read_ds9(region_file)
 wcs_ratio = WCS(hdu_R_PB_corr)
 
 mask_ratio = (regions[0].to_pixel(wcs_ratio)).to_mask()
+N10_cutout = mask_ratio.cutout(N10)
+PB10_cutout = mask_ratio.cutout(PB_10)
+#
 ratio_cutout = mask_ratio.cutout(Ratio)
 ratio_pb_corr_cutout = mask_ratio.cutout(Ratio_pb_corr)
 #
 my_mask = (mask_ratio.data == 1)
-# ratio_streamer = ratio_cutout[gd]
-# ratio_pb_corr_streamer = ratio_pb_corr_cutout[gd]
-
-ratio_cutout_mean = np.mean(ratio_cutout[my_mask == 1])
-ratio_pb_corr_cutout_mean = np.mean(ratio_pb_corr_cutout[my_mask == 1])
-
-A_beam_pix = np.pi * hd8['BMAJ'] * hd8['BMIN'] / (4 * hd8['CDELT1'] * hd8['CDELT1'])
-N_pix = np.sum(my_mask)/A_beam_pix
-
-ratio_cutout_std = np.std(ratio_cutout[my_mask ==1.])
-ratio_pb_corr_cutout_std = np.std(ratio_pb_corr_cutout[my_mask ==1.])
+# <N_10> and <N_10>_PB_corr
+N10_cutout_mean = np.mean(N10_cutout[my_mask])
+N10_pb_cutout_mean = np.mean(N10_cutout[my_mask] / PB10_cutout[my_mask])
+print("Mean col={0:3.2}x10^13, Mean col PB corr={1:3.2}x10^13".format(N10_cutout_mean*1e-13/Pop_Up,
+       N10_pb_cutout_mean*1e-13/Pop_Up))
+print("Difference of {0:2.0} %".format((N10_cutout_mean - N10_pb_cutout_mean) * 100 / N10_cutout_mean))
+#
+ratio_cutout_mean = np.mean(ratio_cutout[my_mask])
+ratio_pb_corr_cutout_mean = np.mean(ratio_pb_corr_cutout[my_mask])
+#
+A_pix = hd8['CDELT1'] * hd8['CDELT1'] * u.deg**2
+A_beam = np.pi * hd8['BMAJ'] * hd8['BMIN'] / 4 * u.deg**2
+N_pix = (np.sum(my_mask) * A_pix / A_beam).decompose()
+#
+ratio_cutout_std = np.std(ratio_cutout[my_mask])
+ratio_pb_corr_cutout_std = np.std(ratio_pb_corr_cutout[my_mask])
 ratio_cutout_mean_err = ratio_cutout_std/np.sqrt(N_pix)
 ratio_pb_corr_cutout_mean_err = ratio_pb_corr_cutout_std/np.sqrt(N_pix)
 
@@ -82,18 +92,22 @@ print(np.around(ratio_cutout_mean, decimals=2), np.around(ratio_cutout_mean_err,
 print(np.around(ratio_pb_corr_cutout_mean, decimals=2), np.around(ratio_pb_corr_cutout_mean_err, decimals=2))
 
 
-Pop_Up = 1.963E-02
-N_10_9_sm_mean/Pop_Up / u.cm**2
-Area_pix = ( np.abs(hd_10_9_sm['CDELT1']*hd_10_9_sm['CDELT1'])*u.deg**2 * distance**2).to(u.pc**2, equivalencies=u.dimensionless_angles())
+N_tot_mean = N10_cutout_mean / Pop_Up / u.cm**2
+N_tot_mean_pb = N10_pb_cutout_mean / Pop_Up / u.cm**2
+Area_pix = (A_pix.to(u.rad**2) * (distance*u.pc)**2).to(u.pc**2, equivalencies=u.dimensionless_angles())
 
-N_total_cutout_sum= (np.sum((N_10_9_sm_cutout[my_mask ==1]))/u.cm**2 * Area_pix / Pop_Up).decompose()
-print( '{0:.2e}'.format(N_total_cutout_sum))
+N_tot_cutout_sum = (np.sum(N10_cutout[my_mask]) * Area_pix / Pop_Up).decompose()
+N_tot_pb_cutout_sum = (np.sum(N10_cutout[my_mask] / PB10_cutout[my_mask]) * Area_pix / Pop_Up).decompose()
+print('{0:.2e}'.format(N_tot_cutout_sum))
 X_HC3N_min = 3.0e-10
 X_HC3N_max = 2.8e-9
 
 from astropy.constants import m_p, G
+mu_mass = 2.8
+M_total_min = (N_tot_cutout_sum * mu_mass * m_p).to(u.Msun) / X_HC3N_max
+M_total_max = (N_tot_cutout_sum * mu_mass * m_p).to(u.Msun) / X_HC3N_min
+print(np.around(M_total_min, decimals=3), np.around(M_total_max, decimals=3))
 
-M_total_min = (N_total_cutout_sum * 2*m_p).to(u.Msun) / X_HC3N_max
-M_total_max = (N_total_cutout_sum * 2*m_p).to(u.Msun) / X_HC3N_min
-print( np.around(M_total_min, decimals=3), np.around(M_total_max, decimals=3))
-
+M_total_pb_min = (N_tot_pb_cutout_sum * mu_mass * m_p).to(u.Msun) / X_HC3N_max
+M_total_pb_max = (N_tot_pb_cutout_sum * mu_mass * m_p).to(u.Msun) / X_HC3N_min
+print(np.around(M_total_pb_min, decimals=3), np.around(M_total_pb_max, decimals=3))
